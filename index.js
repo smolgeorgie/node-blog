326262db5bd56c09d64f6dfb3755497418e7b58a
smolgeorgie <144005439+smolgeorgie@users.noreply.github.com>
2024-11-28 17:59:52 +0100

    loaded in, everything works

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
-import express from "express";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const indexPath = join(__dirname, "index.ejs");
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// Set the view engine to EJS
-app.set("view engine", "ejs");
+// Initialize blog list
+let blogList = [];
 
-// Render index page
-app.get("/", (req, res) => {
-  res.render(indexPath);
+// Function to generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 10000);
+}
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/home', (req, res) => {
+  const blogTitle = req.body.blogTitle;
+  const blogDescription = req.body.blogDes;
+  blogList.push({
+    id: generateID(),
+    title: blogTitle,
+    description: blogDescription,
+  });
+  res.redirect('/bloglist');
+});
+
+app.get('/bloglist', (req, res) => {
+  res.render('blogList', { blogList: blogList });
+});
+
+app.get('/blogDetails/:id', (req, res) => {
+  const blogId = req.params.id;
+  const blogDetails = blogList.find((blog) => blog.id === parseInt(blogId));
+  res.render('blogDetails', { blogDetails: blogDetails });
+});
+
+app.post('/edit/:id', (req, res) => {
+  const blogId = req.params.id;
+  const editBlog = blogList.findIndex((blog) => blog.id === parseInt(blogId));
+  if (editBlog === -1) {
+    res.send('<h1> Something went wrong </h1>');
+  }
+  const updatedTitle = req.body.blogTitle;
+  const updatedDescription = req.body.blogDes;
+
+  blogList[editBlog].title = updatedTitle;
+  blogList[editBlog].description = updatedDescription;
+
+  res.redirect('/bloglist');
+});
+
+app.post('/delete/:id', (req, res) => {
+  const blogId = req.params.id;
+  blogList = blogList.filter((blog) => blog.id !== parseInt(blogId));
+  res.redirect('/bloglist');
 });
 
 // Start the server
